36103e71f09548a46eb6cafde5610f84363566e2
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-02-27 16:37:42 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true, // ยอมรับทุก Host header
+    public: 'https://<your-project-name>.up.railway.app' // หรือโดเมนของคุณถ้ามี
+  }
 })
