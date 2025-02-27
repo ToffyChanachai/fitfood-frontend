4bcf4c28fc160666e294c80e0c29cb9409fb618d
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-02-27 17:32:46 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: process.env.PORT || 8080, // ใช้พอร์ตที่กำหนดใน ENV หรือพอร์ต 8080
-  }
+  transpileDependencies: true
 })
