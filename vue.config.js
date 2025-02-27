bb2749b1d22b4d2662f938e4e6da260c6cbb9564
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-02-27 16:44:06 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true, // ยอมรับทุก Host header
-    public: 'https://<your-project-name>.up.railway.app' // หรือโดเมนของคุณถ้ามี
+    disableHostCheck: true,
+    allowedHosts: 'all',  // ยอมรับทุก Host header
+    public: 'https://fitfood-frontend-production.up.railway.app' // หรือโดเมนของคุณถ้ามี
   }
 })
