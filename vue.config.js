e9d3f8fddc889f8ae91187bb04f686a95aae9b04
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-02-27 16:48:26 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
-    allowedHosts: 'all',  // ยอมรับทุก Host header
-    public: 'https://fitfood-frontend-production.up.railway.app' // หรือโดเมนของคุณถ้ามี
+    disableHostCheck: true,  // ยอมรับทุก Host header
+    allowedHosts: 'all',     // อนุญาตให้เข้าถึงจากทุกๆ host
+    host: '0.0.0.0',         // หรือระบุ host ที่คุณต้องการ เช่น 'localhost' หรือ '0.0.0.0'
+    port: 8080,              // กำหนดพอร์ตที่ต้องการใช้
+    https: false             // ถ้าคุณไม่ต้องการใช้ HTTPS
   }
 })
