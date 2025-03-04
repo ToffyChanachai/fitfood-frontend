1968f41c8c46cf3d84e2f4eebdd9ee76ffbbc749
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-03-04 10:56:31 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/', // ตั้งค่าให้เหมาะสมกับการ deploy
+  devServer: {
+    historyApiFallback: true, // รองรับการใช้ history mode ใน Vue Router
+  },
 })
