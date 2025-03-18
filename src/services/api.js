e2c0198c7e69fcb98687eb235caf4926c2c14986
ToffyChanachai <toffy.chanachai123@gmail.com>
2025-03-18 10:28:46 +0700

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://fitfood-backend.onrender.com";
-export const API_URL = "http://127.0.0.1:3333";
+export const API_URL = "https://fitfood-backend.onrender.com";
+//export const API_URL = "http://127.0.0.1:3333";
